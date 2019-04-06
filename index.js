abccccf45639e3842c213f2fe83802966a4e2590
Jordan Reed <2756092+chanomie@users.noreply.github.com>
2019-04-05 21:25:48 -0700

    Updated, but getting an error on put for some reason

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var request = require("request"),
     Service,
-    Characteristic,
-    MyQDoorState;
+    Characteristic;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
@@ -20,6 +19,8 @@ function MyQ(log, config) {
   this.loglevel         = config["loglevel"];
   this.devices          = [];
 };
+
+// open, opening, closed, closing
 
 MyQ.prototype = {
   /**
@@ -71,18 +72,19 @@ MyQ.prototype = {
       if(deviceDetails["items"][i]["device_family"] == "garagedoor") {
         var newDevice = new MyQGarage(self, self.log);
         newDevice.model = "garagedoor";
+        newDevice.myQ = self;
         newDevice.serialNumber = deviceDetails["items"][i]["serial_number"];
         newDevice.myQDeviceId = deviceDetails["items"][i]["serial_number"];
         newDevice.deviceId = deviceDetails["items"][i]["serial_number"];
         newDevice.name  = deviceDetails["items"][i]["name"];
-        newDevice.targetdoorstate = deviceDetails["items"][i]["state"]["door_state"];
-        newDevice.lastdoorstate = deviceDetails["items"][i]["state"]["door_state"];
         newDevice.doorstate = deviceDetails["items"][i]["state"]["door_state"];
+        newDevice.targetdoorstate = deviceDetails["items"][i]["state"]["door_state"];
         self.devices.push(newDevice);
       } else if(deviceDetails["items"][i]["device_family"] == "gateway") {
         var newDevice = new MyQHub(self, self.log);
         newDevice.name = deviceDetails["items"][i]["name"];
         newDevice.model = "Gateway";
+        newDevice.myQ = self;
         newDevice.serialNumber = deviceDetails["items"][i]["serial_number"];
         newDevice.displayName = deviceDetails["items"][i]["name"];
         newDevice.deviceId = deviceDetails["items"][i]["serial_number"];
@@ -210,129 +212,6 @@ MyQ.prototype = {
         }
       })
     if(self.loglevel > 1) self.log("getDevices: method complete");  	
-  },
-  
-  getDeviceAttribute: function(myQDeviceId, attributeName, callback) {
-  	/*
-    var self = this;
-        
-    self.log("getDeviceAttribute: starting.");
-    
-    var queryString = {
-      'appId': self.MyQApplicationId,
-      'SecurityToken': self.SecurityToken,
-      'myQDeviceId': myQDeviceId,
-      'attributeName': attributeName
-    };
-    try {
-      request({
-        "url": "https://myqexternal.myqdevice.com/api/v4/deviceattribute/getdeviceattribute",
-        'qs': queryString,
-        "method": "GET",
-        "headers": {
-          'Content-Type': 'application/json',
-          'User-Agent': self.UserAgent,
-          'MyQApplicationId': self.MyQApplicationId,
-          'SecurityToken': self.SecurityToken,
-          'BrandId': self.BrandId,
-          'Culture': self.culture
-        }
-      }, function(error, response, body) {
-        try {
-          self.log("getDeviceAttribute: got api response.");
-          var jsonBody = JSON.parse(body);
-          if(self.loglevel >= 3) {
-            self.log("error: " + error);
-            self.log("response: " + response);
-            self.log("body: " + body);
-          };
-          
-          if(jsonBody["ReturnCode"] == "-3333") {
-            // "-3333" means the token has expired and needs to login again and
-            // then retry this method.
-            self.log("getDeviceAttribute: got '-3333' so trying again.");
-            validatewithculture(function() {
-              self.getDeviceAttribute(myQDeviceId, attributeName, callback);
-            });
-          } else {
-            self.log("getDeviceAttribute: got results, checking callback");
-            if(callback) {
-              self.log("getDeviceAttribute: making callback");
-              callback(jsonBody);
-            }
-          }  
-        } catch (err) {
-          self.log("getDeviceAttribute: exception in response getDeviceAttribute: " + err.message);
-        }
-      });
-    } catch (err) {
-      self.log("getDeviceAttribute: exception in request getDeviceAttribute: " + err.message);
-    }
-    */
-  },
-  
-  putDeviceAttribute: function(myQDeviceId, attributeName, attributeValue, callback) {
-  	/*
-    var self = this;
-        
-    self.log("putDeviceAttribute: starting.");
-    var queryString = {
-      'appId': self.MyQApplicationId,
-      'SecurityToken': self.SecurityToken
-    };
-    
-    try {
-      request({
-        "url": "https://myqexternal.myqdevice.com/api/v4/DeviceAttribute/PutDeviceAttribute",
-        'qs': queryString,
-        "method": "PUT",
-        "headers": {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'User-Agent': self.UserAgent,
-          'MyQApplicationId': self.MyQApplicationId,
-          'SecurityToken': self.SecurityToken,
-          'BrandId': self.BrandId,
-          'Culture': self.culture
-        },
-        body: "ApplicationId=" + encodeURIComponent(self.MyQApplicationId) 
-              + "&AttributeName=" + encodeURIComponent(attributeName)
-              + "&AttributeValue=" + encodeURIComponent(attributeValue)
-              + "&MyQDeviceId=" + encodeURIComponent(myQDeviceId)
-              + "&SecurityToken=" + encodeURIComponent(self.SecurityToken)
-              + "&format=json&nojsoncallback=1"
-      }, function(error, response, body) {
-        try {
-          self.log("putDeviceAttribute: got api response.");
-          var bodyJson = JSON.parse(body);
-
-          if(self.loglevel >= 3) {
-            self.log("error: " + error);
-            self.log("response: " + response);
-            self.log("body: " + body);
-          };
-          
-          if(jsonBody["ReturnCode"] == "-3333") {
-            // "-3333" means the token has expired and needs to login again and
-            // then retry this method.
-            self.log("putDeviceAttribute: got '-3333' so trying again.");
-            validatewithculture(function() {
-              self.putDeviceAttribute(myQDeviceId, attributeName, attributeValue, callback);
-            });
-          } else {
-            self.log("putDeviceAttribute: got results, checking callback");
-            if(callback) {
-              self.log("putDeviceAttribute: making callback");
-              callback(bodyJson);
-            }       
-          }
-        } catch (err) {
-          self.log("putDeviceAttribute: exception in response putDeviceAttribute: " + err.message);
-        }
-      });
-    } catch (err) {
-      self.log("putDeviceAttribute: exception in request putDeviceAttribute: " + err.message);
-    }
-    */
   }
 };
 
@@ -366,76 +245,143 @@ function MyQGarage(myQ, log) {
 
 
 MyQGarage.prototype = {
-  /*
   getCurrentDoorState: function(callback) {
     var self = this,
-        returnDoorState = DoorState.CLOSING;
+        returnDoorState = DoorState.CLOSED,
+        requestUrl = "https://api.myqdevice.com/api/v5.1/Accounts/" + self.myQ.AccountId 
+                     + "/Devices/" + self.myQDeviceId;
     
-    self.myQ.getDeviceAttribute(self.myQDeviceId, 'doorstate', function(jsonBody) {
-      var returnCode = jsonBody["ReturnCode"];
-      self.log("getCurrentDoorState: return code is [%s]", returnCode);
-      if(jsonBody["ReturnCode"] == "0") {
-        self.doorstate = jsonBody["AttributeValue"];
-        self.log("getCurrentDoorState: value is [%s]", self.doorstate);
-        if(self.doorstate == MyQDoorState.CLOSED) {
-          self.lastdoorstate = MyQDoorState.CLOSED;
-          returnDoorState = DoorState.CLOSED;
-        } else if(self.doorstate == MyQDoorState.OPEN) {
-          self.lastdoorstate = MyQDoorState.OPEN;
-          returnDoorState = DoorState.OPEN;
-        } else if(self.doorstate == MyQDoorState.MOVING) {
-          if(self.lastdoorstate == MyQDoorState.CLOSED) {
-            returnDoorState = DoorState.OPENING;
+    if(self.myQ.loglevel > 1) self.log("getCurrentDoorState: method start");
+    if(self.myQ.loglevel > 1) self.log("getCurrentDoorState: request url: " + requestUrl);
+    
+
+    request({
+      "url": requestUrl,
+      "method": "GET",
+      "headers": {
+        'User-Agent': self.myQ.UserAgent,
+        'MyQApplicationId': self.myQ.MyQApplicationId,
+        'SecurityToken': self.myQ.SecurityToken
+      },
+      "json": true
+      }, function(error, response, body) {
+        try {
+          if(body.state.door_state) {
+            self.doorstate = body.state.door_state;
+            if(self.myQ.loglevel > 1) self.log("getCurrentDoorState: Door State: " 
+                + self.name + " - " + self.doorstate);
+
+            if(self.doorstate == "open") {
+              returnDoorState = DoorState.OPEN;
+            } else if(self.doorstate == "close") {
+              returnDoorState = DoorState.CLOSE;
+            } else if(self.doorstate == "opening") {
+              returnDoorState = DoorState.OPENING;
+            } else if(self.doorstate == "closing") {
+              returnDoorState = DoorState.CLOSING;
+            }
+            
+            if(callback) {
+              callback(null, returnDoorState);
+            }          
           } else {
-            returnDoorState = DoorState.CLOSING;
+            self.log("getCurrentDoorState error: " + error);
+            self.log("getCurrentDoorState response: " + response);
+            self.log("getCurrentDoorState body: " + body);
           }
-        } else {
-          returnDoorState = DoorState.OPEN;
+        } catch (err) {
+            self.log("getCurrentDoorState2 error: " + err);
+            self.log("getCurrentDoorState2 error: " + error);
+            self.log("getCurrentDoorState2 response: " + response);
+            self.log("getCurrentDoorState2 body: " + body);
         }
-      }
-      callback(null, returnDoorState);
-    });
-    
+      })
+    if(self.myQ.loglevel > 1) self.log("getCurrentDoorState: method complete");  	    
   },
+  
   getTargetDoorState: function(callback) {
     var self = this;
     
-    if(self.targetdoorstate == MyQDoorState.CLOSED) {
+    if(self.targetdoorstate == "closed") {
       callback(null,DoorState.CLOSED);
-    } else if(self.targetdoorstate == MyQDoorState.OPEN) {
+    } else if(self.targetdoorstate == "open") {
       callback(null,DoorState.OPEN);
     } else {
       callback(null, DoorState.CLOSED);  
     }
   },
   setTargetDoorState: function(targetState, callback) {
-    var self = this;
+    var self = this,    
+        requestUrl = "https://api.myqdevice.com/api/v5.1/Accounts/" + self.myQ.AccountId 
+                     + "/Devices/" + self.myQDeviceId + "/actions";
     
-    if (targetState == DoorState.OPEN && self.doorstate == MyQDoorState.CLOSED) {
-      self.log("setTargetDoorState: Request to open a closed door.");
-      self.targetdoorstate = MyQDoorState.OPEN;
-      self.myQ.putDeviceAttribute(self.myQDeviceId, 'desireddoorstate', MyQDoorState.TARGET_OPEN, function(jsonBody) {
-        if(jsonBody["ReturnCode"] == "0") {
-          callback(null);
+    if (targetState == DoorState.OPEN && self.doorstate == "closed") {
+      if(self.myQ.loglevel > 0) self.log("setTargetDoorState: Request to open a closed door.");
+      if(self.myQ.loglevel > 1) self.log("setTargetDoorState: URL: " + requestUrl)
+      self.targetdoorstate = "open";
+
+      request({
+        "url": requestUrl,
+        "method": "PUT",
+        "headers": {
+          'User-Agent': self.myQ.UserAgent,
+          'Content-Type': 'application/json',          
+          'MyQApplicationId': self.myQ.MyQApplicationId,
+          'SecurityToken': self.myQ.SecurityToken,
+          "body": {'action_type':'open'}
+          },
+        "json": true
+        }, function(error, response, body) {
+            // self.log("getCurrentDoorState error: " + JSON.stringify(error));
+            self.log("getCurrentDoorState response: " + JSON.stringify(response));
+            self.log("getCurrentDoorState body: " + JSON.stringify(body));
+          if(error) {
+            self.log("getCurrentDoorState error: " + error);
+            self.log("getCurrentDoorState response: " + response);
+            self.log("getCurrentDoorState body: " + body);
+          } else {
+            callback(null)
+          }
         }
-      });
-    } else if (targetState == DoorState.CLOSED && self.doorstate == MyQDoorState.OPEN) {
-      self.log("setTargetDoorState: Request to close an open door.");
-      self.targetdoorstate = MyQDoorState.CLOSED;
-      self.myQ.putDeviceAttribute(self.myQDeviceId, 'desireddoorstate', MyQDoorState.TARGET_CLOSED, function(jsonBody) {
-        if(jsonBody["ReturnCode"] == "0") {
-          callback(null);
+      );
+            
+    } else if (targetState == DoorState.CLOSED && self.doorstate == "open") {
+      if(self.myQ.loglevel > 0) self.log("setTargetDoorState: Request to close an open door.");
+      if(self.myQ.loglevel > 1) self.log("setTargetDoorState: URL: " + requestUrl)      
+      self.targetdoorstate = "closed";
+
+      request({
+        "url": requestUrl,
+        "method": "PUT",
+        "headers": {
+          'User-Agent': self.myQ.UserAgent,
+          'Content-Type': 'application/json',          
+          'MyQApplicationId': self.myQ.MyQApplicationId,
+          'SecurityToken': self.myQ.SecurityToken,
+          "body": {'action_type':'close'}
+          },
+          "json": true
+        }, function(error, response, body) {
+            self.log("getCurrentDoorState error: " + error);
+            self.log("getCurrentDoorState response: " + response);
+            self.log("getCurrentDoorState body: " + body);
+          if(error) {
+            self.log("getCurrentDoorState error: " + error);
+            self.log("getCurrentDoorState response: " + response);
+            self.log("getCurrentDoorState body: " + body);
+          } else {
+            callback(null)
+          }
         }
-      });
+      );    
+
     }
   },
-  */
   getServices: function() {
     var self = this,
         garageService = new Service.GarageDoorOpener(this.name),
         informationService = new Service.AccessoryInformation();
     
-    /*
     garageService
       .getCharacteristic(Characteristic.CurrentDoorState)
       .on('get', this.getCurrentDoorState.bind(this));
@@ -449,7 +395,6 @@ MyQGarage.prototype = {
     informationService.setCharacteristic(Characteristic.Model, this.model);
     informationService.setCharacteristic(Characteristic.Name, this.deviceId);
     informationService.setCharacteristic(Characteristic.SerialNumber, this.serialNumber);
-    */
             
     return [informationService, garageService];
   }
